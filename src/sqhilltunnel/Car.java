e7a938441894b573233db0b6f3d9a8b3ecaf0e84
drewmcd621 <drewmcd621@bitbucket.org>
2013-11-23 11:11:28 -0500

    Setup

--- src/sqhilltunnel/Car.java
+++ src/sqhilltunnel/Car.java
@@ -2,6 +2,42 @@
 package sqhilltunnel;
 
 
-public class Car {
+public class Car  implements Comparable<Car>
+{
+    private int distance;
+    
+    public void Accelerate()
+    {
+        
+    }
+    public void Slowdown()
+    {
+        
+    }
+    public void RandomSlow()
+    {
+        
+    }
+    public void Move()
+    {
+        
+    }
+
+    @Override
+    public int compareTo(Car o) 
+    {
+        if(distance > o.distance)
+        {
+            return 1;
+        }
+        else if (distance < o.distance)
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 
 }
